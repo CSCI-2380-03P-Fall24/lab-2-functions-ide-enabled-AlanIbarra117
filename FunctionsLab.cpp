b3baeda89ee844f3aae0a51259927bb31006847a
AlanIbarra117 <148792188+AlanIbarra117@users.noreply.github.com>
2024-09-09 03:39:35 +0000

    finished lab2

--- FunctionsLab.cpp
+++ FunctionsLab.cpp
@@ -57,23 +57,3 @@ int sumOdds(int array[], int start, int end) {
     }
     return sum;
 }
-int main(){  
-    int arr[] = {10, 50, 5, 7, 9, 2, 100, 2, 40};
-    int size = sizeof(arr) / sizeof(arr[0]);
-
-    int minIndex = locateSmallest(arr, 0, 5);
-    cout << "Index of the smallest element between 0 and 5: " << minIndex << endl; 
-
-    cout << "Array before replacement: ";
-    printArray(arr, size);
-    
-    replaceVariable(arr, 2, 0, size - 1);
-    
-    cout << "Array after replacing 2 with 1000: ";
-    printArray(arr, size);
-
-    int oddSum = sumOdds(arr, 0, size - 1);
-    cout << "Sum of odd numbers in the array: " << oddSum << endl; 
-
-    return 0;
-}
--- demo.cpp
+++ demo.cpp
@@ -1,6 +1,26 @@
 // write any code to test out your functions here
 // run comman "make demo" to run this code
 
+#include <iostream>
 int main() {
+    int arr[] = {10, 50, 5, 7, 9, 2, 100, 2, 40};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    int minIndex = locateSmallest(arr, 0, 5);
+    cout << "Index of the smallest element between 0 and 5: " << minIndex << endl; // Output: 5 (index of 2)
+
+   
+    cout << "Array before replacement: ";
+    printArray(arr, size);
+    
+    replaceVariable(arr, 2, 0, size - 1);
+    
+    cout << "Array after replacing 2 with 1000: ";
+    printArray(arr, size); // Output: 10 50 5 7 9 1000 100 1000 40
+
+    
+    int oddSum = sumOdds(arr, 0, size - 1);
+    cout << "Sum of odd numbers in the array: " << oddSum << endl; // Output: 5 + 7 + 9 = 21
+
     return 0;
 }
